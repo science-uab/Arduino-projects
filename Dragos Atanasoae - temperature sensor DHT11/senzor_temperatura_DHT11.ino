3e0530976da1d2426a3765313f14b8359a535857
Dragos Atanasoae <dragossh86eu@gmail.com>
2018-06-02 12:09:29 +0300

    initial commit DHT11

---
+++ Dragos Atanasoae - temperature sensor DHT11/senzor_temperatura_DHT11.ino
@@ -0,0 +1,34 @@
+
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 7
+#define RED_LED_PIN 12
+#define GREEN_LED_PIN 11
+
+
+void setup(){
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+
+  if(DHT.temperature > 28){
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+  }else {
+    digitalWrite(GREEN_LED_PIN, HIGH);
+    digitalWrite(RED_LED_PIN, LOW);
+  }
+  
+  delay(1000);
+}
